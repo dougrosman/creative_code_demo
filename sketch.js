e65f111cf0b6da0744cd06b837bf618891d72be3
Doug Rosman <dougrosman@gmail.com>
2024-08-28 13:43:02 -0500

    add all the files

---
+++ sketch.js
@@ -0,0 +1,14 @@
+console.log("this message is from sketch.js")
+
+function setup() {
+    createCanvas(400, 400);
+    background(0)
+}
+
+function draw() {
+
+}
+
+function mouseMoved() {
+    rect(mouseX, mouseY, 20, 20);
+}
